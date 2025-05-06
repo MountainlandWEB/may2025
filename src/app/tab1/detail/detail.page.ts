4af188d560ab298459c13634fbf1799419fe9b46
Todd H <github@simplybest.com>
2025-05-05 20:10:12 -0600

    pass person to detail page

--- src/app/tab1/detail/detail.page.ts
+++ src/app/tab1/detail/detail.page.ts
@@ -1,24 +1,21 @@
-import { Component, inject, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar, IonButtons, IonBackButton } from '@ionic/angular/standalone';
-import { ActivatedRoute } from '@angular/router';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonButtons, IonBackButton, IonCardSubtitle, IonCardContent, IonCardTitle, IonCardHeader, IonCard } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-detail',
   templateUrl: './detail.page.html',
   styleUrls: ['./detail.page.scss'],
   standalone: true,
-  imports: [IonBackButton, IonButtons, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [ IonCard, IonCardHeader, IonCardTitle, IonCardContent, IonCardSubtitle, IonBackButton, IonButtons, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule]
 })
 export class DetailPage {
 
-  activatedRoute = inject(ActivatedRoute);
   person: any;
 
   constructor() { }
 
-  onViewWillEnter() {
-    this.person = this.activatedRoute.snapshot.data['person'];
+  ionViewWillEnter() {
+    this.person = history.state.data;
   }
 }
--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,19 +1,21 @@
 import { Component, inject } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent, IonList, IonItem, IonLabel, IonButtons, IonIcon, IonButton, ModalController } from '@ionic/angular/standalone';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonList, IonItem, IonButtons, IonIcon, IonButton, ModalController, IonLabel } from '@ionic/angular/standalone';
 import { addIcons } from 'ionicons';
 import { informationCircleOutline } from 'ionicons/icons';
 import { AboutPage } from '../about/about.page';
 import { CommonModule } from '@angular/common';
-import { RouterModule } from '@angular/router';
+import { Router, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss'],
-  imports: [RouterModule, CommonModule, IonButton, IonIcon, IonButtons, IonLabel, IonItem, IonList, IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [IonLabel, RouterModule, CommonModule, IonButton, IonIcon, IonButtons, IonItem, IonList, IonHeader, IonToolbar, IonTitle, IonContent],
 })
 export class Tab1Page {
   aboutCtrl = inject(ModalController);
+  router = inject(Router);
+
   persons = [
     { id: 1, name: 'Carlos', age: 25 },
     { id: 2, name: 'Ana', age: 30 },
@@ -25,6 +27,10 @@ export class Tab1Page {
 
   constructor() {
     addIcons({ informationCircleOutline });
+  }
+
+  loadDetail(person: any) {
+    this.router.navigate(['/tabs/tab1/detail'], { state: { data: person } });
   }
 
   async openAbout() {
