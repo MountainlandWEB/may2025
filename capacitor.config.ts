3ee14fa69fb07377fac3fb46379549ecf8db333b
Todd H <github@simplybest.com>
2025-05-06 18:39:51 -0600

    add ios and android and tweak tabs

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'edu.mtec.mtechsports',
+  appName: 'MTech Sports',
+  webDir: 'www'
+};
+
+export default config;
--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,20 +1,33 @@
 import { Component, inject } from '@angular/core';
 import { Router, RouterModule } from '@angular/router';
-import { IonHeader, IonToolbar, IonTitle, IonContent, IonButton } from '@ionic/angular/standalone';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonButton, IonButtons, IonIcon } from '@ionic/angular/standalone';
+import { addIcons } from 'ionicons';
+import { shareOutline } from 'ionicons/icons';
+import { Share } from '@capacitor/share';
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss'],
-  imports: [RouterModule, IonButton, IonHeader, IonToolbar, IonTitle, IonContent]
+  imports: [IonIcon, IonButtons, RouterModule, IonButton, IonHeader, IonToolbar, IonTitle, IonContent]
 })
 export class Tab2Page {
   router = inject(Router);
 
-  constructor() {}
+  constructor() {
+    addIcons({ shareOutline})
+  }
 
   goToTab1() {
     this.router.navigate(['/tabs/tab1']);
   }
 
+  async openShare() {
+    await Share.share({
+      title: 'See cool stuff',
+      text: 'Really awesome thing you need to see right now',
+      url: 'https://mtechsports2025.web.app/',
+      dialogTitle: 'Share Mtech Sports',
+    });
+  }
 }
